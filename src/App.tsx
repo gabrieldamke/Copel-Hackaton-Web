31ba70b861356df2ad124a873a597d2fee7e79a7
gabrieldamke <75023146+gabrieldamke@users.noreply.github.com>
2022-11-27 03:32:09 -0300

    add new locations improve location service

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,11 @@ import SendIcon from '@mui/icons-material/Send';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { Button, Grid, MenuItem, Modal, Select } from '@mui/material';
 import "leaflet/dist/leaflet.css";
+import Lottie from 'react-lottie';
+import animationData from './lotties/50186-task-complete-tick.json';
+import incorrectAnimationData from './lotties/91878-bouncy-fail.json'
 
-
-//L.Marker.prototype.options.icon = DefaultIcon TODO
 const L = require('leaflet');
-
 const myIcon = L.icon({
   iconUrl: require('./shadowicon.png'),
   iconSize: [64, 64],
@@ -41,17 +41,87 @@ const myIcon = L.icon({
 });
 const drawerWidth = 240;
 function App() {
-  const [cidade, setCidade] = useState<String>('Curitiba');
+  const [cidade, setCidade] = useState<String>('Guarapuava');
   const [bairro, setBairro] = useState<String>('Centro');
-  const [unidadeConsumidora, setUnidadeConsumidora] = useState<String>('000000001');
+  const [unidadeConsumidora, setUnidadeConsumidora] = useState<Number>(1);
   const [transformador, setTransformador] = useState<String>();
   const [isCidadeModalOpen, setIsCidadeModalOpen] = useState<boolean>(false);
   const [isBairroModalOpen, setIsBairroModalOpen] = useState(false);
   const [isUnidadeTransformadoraOpen, setIsUnidadeTransformadoraOpen] = useState<boolean>(false);
   const [isAnalisarDrawerOpen, setIsAnalisarDrawerOpen] = useState<boolean>(false);
   const [isTransformadorModalOpen, setIsTransformadorModalOpen] = useState(false);
-  const [isGato, setIsGato] = useState<boolean>(false);
+  const [isGato, setIsGato] = useState<boolean>(true);
+  const [latLong, setLatLong] = useState<[number, number]>([-25.3935, -51.4562]);
+  function FlyMapTo() {
+
+    const map = useMap()
+
+    useEffect(() => {
+      map.flyTo(latLong)
+    }, [latLong])
+
+    return null
+  }
+
+
+  const guarapuava = [
+    {
+      latitude: -25.3935,
+      longitude: -51.4562
+    },
+    {
+      latitude: -25.395270,
+      longitude: -51.489320
+    },
+    {
+      latitude: -25.386590,
+      longitude: -51.505720
+    },
+    {
+      latitude: -25.38007,
+      longitude: -51.47676
+    },
+    {
+      latitude: -25.4001,
+      longitude: -51.4559
+    },
+
+  ]
+
+  const curitiba = [
+    {
+      latitude: -25.44572,
+      longitude: -49.2642
+    },
+    {
+      latitude: -25.44153,
+      longitude: -49.2669
+    },
+    {
+      latitude: -25.411205,
+      longitude: -49.291823
+    },
+    {
+      latitude: -25.491363,
+      longitude: -49.14466
+    },
+    {
+      latitude: -25.404733,
+      longitude: -49.187162
+    },
+
+  ]
+  const handleCoordinateChange = (latitude: number, longitude: number, city: string) => {
+    const temp: [number, number] = [latitude, longitude]
+    console.log('setei latlong' + temp);
+    setLatLong(temp);
+  }
   const handleCidade = () => {
+    if (cidade === 'Guarapuava') {
+      handleCoordinateChange(guarapuava[Number(unidadeConsumidora) - 1].latitude, guarapuava[Number(unidadeConsumidora) - 1].longitude, 'Guarapuava')
+    } if (cidade === 'Curitiba') {
+      handleCoordinateChange(curitiba[Number(unidadeConsumidora) - 1].latitude, curitiba[Number(unidadeConsumidora) - 1].longitude, 'Curitiba')
+    }
     setIsCidadeModalOpen(true);
     setIsAnalisarDrawerOpen(false);
   }
@@ -60,6 +130,11 @@ function App() {
     setIsAnalisarDrawerOpen(false);
   }
   const handleBairro = () => {
+    if (cidade === 'Guarapuava') {
+      handleCoordinateChange(guarapuava[Number(unidadeConsumidora) - 1].latitude, guarapuava[Number(unidadeConsumidora) - 1].longitude, 'Guarapuava')
+    } else if (cidade === 'Curitiba') {
+      handleCoordinateChange(curitiba[Number(unidadeConsumidora) - 1].latitude, curitiba[Number(unidadeConsumidora) - 1].longitude, 'Curitiba')
+    }
     setIsBairroModalOpen(true);
     setIsAnalisarDrawerOpen(false);
   }
@@ -67,7 +142,13 @@ function App() {
     setIsBairroModalOpen(false);
   }
   const handleUnidadeConsumidora = () => {
+    if (cidade === 'Guarapuava') {
+      handleCoordinateChange(guarapuava[Number(unidadeConsumidora) - 1].latitude, guarapuava[Number(unidadeConsumidora) - 1].longitude, 'Guarapuava')
+    } else if (cidade === 'Curitiba') {
+      handleCoordinateChange(curitiba[Number(unidadeConsumidora) - 1].latitude, curitiba[Number(unidadeConsumidora) - 1].longitude, 'Curitiba')
+    }
     setIsUnidadeTransformadoraOpen(true);
+    setIsAnalisarDrawerOpen(false);
   }
   const handleUnidadeConsumidoraClose = () => {
     setIsUnidadeTransformadoraOpen(false);
@@ -75,6 +156,11 @@ function App() {
 
   const handleAnalisar = () => {
     setIsAnalisarDrawerOpen(true);
+    if (cidade === 'Guarapuava') {
+      handleCoordinateChange(guarapuava[Number(unidadeConsumidora) - 1].latitude, guarapuava[Number(unidadeConsumidora) - 1].longitude, 'Guarapuava')
+    } else if (cidade === 'Curitiba') {
+      handleCoordinateChange(curitiba[Number(unidadeConsumidora) - 1].latitude, curitiba[Number(unidadeConsumidora) - 1].longitude, 'Curitiba')
+    }
 
   }
   const Boxstyle = {
@@ -88,7 +174,26 @@ function App() {
     boxShadow: 24,
     p: 4,
   };
-  const position = [51.505, -0.09]
+  const defaultOptions = {
+    keepLastFrame: true,
+    autoplay: false,
+    loop: false,
+    speed: 0.5,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+  const IncorrectdefaultOptions = {
+    keepLastFrame: true,
+    autoplay: false,
+    loop: false,
+    speed: 0.5,
+    animationData: incorrectAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
 
     <div /*style={{ backgroundImage: `url(/lowResBackground.png)` }} */ >
@@ -112,7 +217,6 @@ function App() {
         anchor="left"
       >
         <img src='/copel.png' height={'80px'} width={'200px'} />
-
         <Divider />
         <List>
           <ListItem>
@@ -164,37 +268,32 @@ function App() {
         anchor="right"
       >
         <Grid container margin={2}>
-          <Grid item>
-            <Divider />
-            <List>
-              <ListItem>
-                <ListItemButton onClick={handleCidade}>
-                  <ListItemIcon ><LocationCityIcon /></ListItemIcon>
-                  <ListItemText disableTypography
-                    primary={<Typography variant="body2" style={{ color: '#FFFFFF' }}>{cidade}</Typography>} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton onClick={handleBairro}>
-                  <ListItemIcon ><HouseIcon /></ListItemIcon>
-                  <ListItemText primary={<Typography variant="body2" style={{ color: '#FFFFFF' }}>{bairro}</Typography>} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton onClick={handleUnidadeConsumidora}>
-                  <ListItemIcon ><ElectricBoltIcon /></ListItemIcon>
-                  <ListItemText primary={<Typography variant="body2" style={{ color: '#FFFFFF' }}>{unidadeConsumidora}</Typography>} />
-                </ListItemButton>
-              </ListItem>
-            </List>
-
+          <Grid item justifySelf={'center'} style={{ textAlign: "center" }} justifyContent={'center'}>
+            <Typography align={'center'} variant="h6" style={{ fontWeight: 'bold', color: '#FFFFFF' }}>CIDADE</Typography>
+            <Typography variant="h6" style={{ color: '#FFFFFF' }}>{cidade}</Typography>
+            <Typography align={'center'} variant="h6" style={{ fontWeight: 'bold', color: '#FFFFFF' }}>BAIRRO</Typography>
+            <Typography variant="h6" style={{ color: '#FFFFFF' }}>{bairro}</Typography>
+            <Typography align={'center'} variant="h6" style={{ fontWeight: 'bold', color: '#FFFFFF' }}>UNIDADE CONSUMIDORA</Typography>
+            <Typography variant="h6" style={{ color: '#FFFFFF' }}>{unidadeConsumidora.toString().padStart(8, '0')}</Typography>
             <Divider />
           </Grid>
           <br />
           <Grid item>
-            {!isGato && <img src={"/correct.png"} width={'200px'}></img>}
+            {!isGato && <Lottie
+
+              options={defaultOptions}
+              height={120}
+              width={120}
+            />}
+            {!isGato && <Typography variant="h6" style={{ color: '#FFFFFF' }}>Unidade consumidora segura</Typography>}
             <Divider />
-            <Typography variant="h6" style={{ color: '#FFFFFF' }}>Unidade consumidora segura</Typography>
+            {isGato && <Lottie
+
+              options={IncorrectdefaultOptions}
+              height={120}
+              width={120}
+            />}
+            {isGato && <Typography variant="h6" style={{ color: '#FFFFFF' }}>Possível fraude de energia!</Typography>}
             <Divider />
           </Grid>
         </Grid>
@@ -218,6 +317,7 @@ function App() {
             onChange={(e) => setCidade(e.target.value as string)}
           >
             <MenuItem value={'Curitiba'}>Curitiba</MenuItem>
+            <MenuItem value={'Guarapuava'}>Guarapuava</MenuItem>
           </Select>
           <Button onClick={handleCidadeClose}>Fechar</Button>
         </Box>
@@ -265,31 +365,31 @@ function App() {
             id="demo-simple-select"
             value={unidadeConsumidora}
             label="Unidade Consumidora"
-            onChange={(e) => setUnidadeConsumidora(e.target.value as string)}
+            onChange={(e) => setUnidadeConsumidora(Number(e.target.value))}
           >
-            <MenuItem value={'000000001'}>000000001</MenuItem>
-            <MenuItem value={'000000002'}>000000002</MenuItem>
-            <MenuItem value={'000000003'}>000000003</MenuItem>
-            <MenuItem value={'000000004'}>000000004</MenuItem>
-            <MenuItem value={'000000005'}>000000005</MenuItem>
+            <MenuItem value={1}>000000001</MenuItem>
+            <MenuItem value={2}>000000002</MenuItem>
+            <MenuItem value={3}>000000003</MenuItem>
+            <MenuItem value={4}>000000004</MenuItem>
+            <MenuItem value={5}>000000005</MenuItem>
           </Select>
           <Button onClick={handleUnidadeConsumidoraClose}>Fechar</Button>
         </Box>
       </Modal>
-      <MapContainer center={[-25.441105, -49.276855]} zoom={13} style={{ height: '100vh', width: '100wh' }} scrollWheelZoom={false}>
+      <MapContainer center={latLong} zoom={13} style={{ height: '100vh', width: '100wh' }} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker icon={myIcon} position={[-25.441105, -49.276855]}>
+        <Marker icon={myIcon} position={latLong}>
           <Popup>
             Localização da unidade consumidora analisada
           </Popup>
         </Marker>
+        <FlyMapTo />
       </MapContainer>
 
     </div >
   );
 }
-
 export default App; 
