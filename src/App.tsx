6c6ef151da4149a0ef9bf85eb703303b1e484161
gabrieldamke <75023146+gabrieldamke@users.noreply.github.com>
2022-11-29 13:45:09 -0300

    last update

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,57 @@ import Lottie from 'react-lottie';
 import animationData from './lotties/50186-task-complete-tick.json';
 import incorrectAnimationData from './lotties/91878-bouncy-fail.json'
 import { Icon } from 'leaflet';
-
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+const data = [
+  {
+    name: '0:00',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: '3:00',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: '6:00',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: '9:00',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: '12:00',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: '15:00',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: '18:00',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: '18:00',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 const L = require('leaflet');
 const myIcon = L.icon({
   iconUrl: require('./shadowicon.png'),
@@ -192,7 +242,7 @@ function App() {
       preserveAspectRatio: "xMidYMid slice"
     }
   };
-  const IncorrectdefaultOptions = { 
+  const IncorrectdefaultOptions = {
     autoplay: false,
     loop: false,
     speed: 0.5,
@@ -393,6 +443,26 @@ function App() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Detalhamento da análise
           </Typography>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+
         </Box>
       </Modal>
       <MapContainer center={latLong} zoom={14} style={{ height: '100vh', width: '100wh' }} scrollWheelZoom={false}>
